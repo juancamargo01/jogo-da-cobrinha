316fc3243d9e9bd80f765947587c3fac6e8b348e
juancamargo01 <74567426+juancamargo01@users.noreply.github.com>
2021-09-08 17:41:15 -0300

    função iniciar jogo criada

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ snake[0] ={
     y: 8 * box
 }
 
+let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -22,5 +28,21 @@ function criarCobrinha (){
 }
 
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){    
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+
+    criarBG();
+    criarCobrinha();
+}
